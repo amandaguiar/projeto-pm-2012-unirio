60949414b5c51a9a644c9185113c0bd4f0a3b7cb
bianca.melo@uniriotec.br <bianca.melo@uniriotec.br@ad2921a3-afa2-7aa1-14f8-43855f1eb3cd>
2012-05-06 13:19:43 +0000

    Criando repositório do projeto...

---
+++ Ola/src/ola/Ola.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ola;
+
+/**
+ *
+ * @author Bianca
+ */
+public class Ola {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Olá mundo!");
+    }
+}
