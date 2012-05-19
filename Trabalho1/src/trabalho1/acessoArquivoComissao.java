a8e099a41aef6bfd19a7a672fd8790e77abec7c9
filipelima1990@gmail.com <filipelima1990@gmail.com@ad2921a3-afa2-7aa1-14f8-43855f1eb3cd>
2012-05-19 15:22:46 +0000

    Criação das funções getVendas, getPrecos e getVendedores que retornam uma lista de acordo com os dados lidos no arquivo da classe correspondente

--- Trabalho1/src/trabalho1/acessoArquivoComissao.java
+++ Trabalho1/src/trabalho1/acessoArquivoComissao.java
@@ -5,12 +5,15 @@
 
 package trabalho1;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  *
- * @author Jean
+ * @author Bianca e Filipe
  */
 public class acessoArquivoComissao extends acessoArquivo{
 
@@ -20,25 +23,66 @@ public class acessoArquivoComissao extends acessoArquivo{
     public static final int POS_COD_VENDEDOR = 1;
     public static final int POS_NOME_VENDEDOR = 2;
     public static final int PRIMEIRA_POS_TOTAL = 3;
-    
+          
 //    Comissao c = new Comissao();
+   
    
     /* Essa função compara o mês entrado com os meses presentes no arquivo de venda.
      * Para cada ocorrência de igualmente, a linha de venda é adicionada em um array.
      */
 
-//    public List<String> getVendasMes(String[] campos) {
+//    public List<String> getVendasNoMes(String[] campos) {
 //        try {
-//            if (c.getMes() ==  /*Mes do arquivo de vendas*/)
-
-                /*Bianca, parei aqui pq eu tô mt cansado! Quando eu acordar eu começo a fazer de novo!
-             * Mas a ideia é o que eu escrevi no comentário mesmo, pega o Mes que o usuário vai entrar,
-                 * compara com os meses do arquivo de venda e traz só as linhas que vão importar pro cálculo,
-                 * ou seja, as linhas do mês que o cara entrar!
-             */
-//        }
-//
-//    }
+//            Scanner input = new Scanner(System.in);
+//            System.out.println("Mes: ");
+//            int mes = input.nextInt();
+//            c.setMes(mes);
+//}  
+    
+    public List<Venda> getVendas() throws FileNotFoundException, acessoArquivoException {
+            acessoArquivo accArqVenda = new acessoArquivoVenda();
+            Scanner input = new Scanner(System.in);
+            System.out.println("Nome do arquivo de vendas: ");
+            String ARQ_VENDAS = input.nextLine();
+         try {
+            List<Venda> listVendas = accArqVenda.ler(new FileReader(new File(ARQ_VENDAS)));
+            return listVendas;       
+        }catch (acessoArquivoException ex) {
+            throw new acessoArquivoException("Erro ao tentar acessar o arquivo de Vendas");
+        }catch (FileNotFoundException ex) {
+            throw new FileNotFoundException("Arquivo nao encontrado");
+    }
+}
+    
+    public List<Preco> getPrecos() throws FileNotFoundException, acessoArquivoException {
+            acessoArquivo accArqPreco = new acessoArquivoPreco();
+            Scanner input = new Scanner(System.in);
+            System.out.println("Nome do arquivo de vendas: ");
+            String ARQ_PRECOS = input.nextLine();
+        try {
+            List<Preco> listPrecos = accArqPreco.ler(new FileReader(new File(ARQ_PRECOS)));
+            return listPrecos;       
+        }catch (acessoArquivoException ex) {
+            throw new acessoArquivoException("Erro ao tentar acessar o arquivo de Precos");
+        }catch (FileNotFoundException ex) {
+            throw new FileNotFoundException("Arquivo nao encontrado");
+    }
+}
+   
+    public List<Vendedor> getVendedores() throws FileNotFoundException, acessoArquivoException {
+            acessoArquivo accArqVendedor = new acessoArquivoVendedor();
+            Scanner input = new Scanner(System.in);
+            System.out.println("Nome do arquivo de vendas: ");
+            String ARQ_VENDEDORES = input.nextLine();
+        try {
+            List<Vendedor> listVendedores = accArqVendedor.ler(new FileReader(new File(ARQ_VENDEDORES)));
+            return listVendedores;       
+    }catch (acessoArquivoException ex) {
+        throw new acessoArquivoException("Erro ao tentar acessar o arquivo de Vendedores");
+    }catch (FileNotFoundException ex) {
+        throw new FileNotFoundException("Arquivo nao encontrado");
+    }
+}
     
     @Override
     public void verificarPreCondicoes(String[] campos) throws acessoArquivoException {
