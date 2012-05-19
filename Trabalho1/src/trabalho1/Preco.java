236b1014cc05ec0cd7f306bec519216bdda787b7
amandinhamotta@gmail.com <amandinhamotta@gmail.com@ad2921a3-afa2-7aa1-14f8-43855f1eb3cd>
2012-05-19 04:55:30 +0000

    Implementei os métodos das classes Preco e acessoArquivoPreco.

--- Trabalho1/src/trabalho1/Preco.java
+++ Trabalho1/src/trabalho1/Preco.java
@@ -5,27 +5,39 @@
 
 package trabalho1;
 
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 import java.util.List;
+
 
 
 /**
  *
- * @author Jean
+ * @author Amanda
  */
-public class Preco {
-    private String data;
+public class Preco {    
+    
+    private GregorianCalendar data;
     private List<Double> precos;
 
-    public Preco(String data, List<Double> precos) {
+    public static final int POS_CAT_A = 0;
+    public static final int POS_CAT_B = 1;
+    public static final int POS_CAT_C = 2;
+    
+    public Preco(GregorianCalendar data, List<Double> precos) {
         this.data = data;
+        this.data.setLenient(false);     
+        this.data.get(Calendar.MONTH);  //Inicializar todos os demais campos da Classe GregorianCalendar
+        
         this.precos = precos;
     }
 
-    public String getData() {
+    public GregorianCalendar getData() {
         return data;
     }
 
-    public void setData(String data) {
+    public void setData(GregorianCalendar data) {
         this.data = data;
     }
 
@@ -36,5 +48,54 @@ public class Preco {
     public void setPrecos(List<Double> precos) {
         this.precos = precos;
     }
+    
+    public int getDia(){
+        return this.data.DAY_OF_MONTH;
+    }
+    
+    public int getMes(){
+        return this.data.MONTH + 1;
+    }
+    
+    public int getAno(){
+        return this.data.YEAR;
+    }
+    
+    public Double getPrecoCatA(){
+        return this.precos.get(POS_CAT_A);
+    }
+    
+    public Double getPrecoCatB(){
+        return this.precos.get(POS_CAT_B);
+    }
+    
+    public Double getPrecoCatC(){
+        return this.precos.get(POS_CAT_C);        
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Preco other = (Preco) obj;
+        if (this.data != other.data && (this.data == null || !this.data.equals(other.data))) {
+            return false;
+        }
+        if (this.precos != other.precos && (this.precos == null || !this.precos.equals(other.precos))) {
+            return false;
+        }
+        return true;
+    }
 
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 61 * hash + (this.data != null ? this.data.hashCode() : 0);
+        hash = 61 * hash + (this.precos != null ? this.precos.hashCode() : 0);
+        return hash;
+    }
 }
--- Trabalho1/src/trabalho1/acessoArquivoPreco.java
+++ Trabalho1/src/trabalho1/acessoArquivoPreco.java
@@ -5,22 +5,86 @@
 
 package trabalho1;
 
+import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.List;
+import java.io.IOException;
+import java.util.ArrayList;
 
 /**
  *
- * @author Jean
+ * @author Amanda
  */
 public class acessoArquivoPreco extends acessoArquivo{
 
+    public static final String DELIMITADOR = ";";  //Delimitador de cada campo no arquivo.
+    public static final int POS_DATA = 0;  //Posição da data.
+    public static final int POS_PRECO_PROD_A = 1; //Posição do preço do produto A no arquivo.
+    public static final int POS_PRECO_PROD_B = 2; //Posição do preço do produto B no arquivo.
+    public static final int POS_PRECO_PROD_C = 3; //Posição do preço do produto C no arquivo.
+    public static final int PRIMEIRA_POSICAO_PRECO = 1; //Posição do primeiro preço no arquivo.
+    public static final int QUANT_CAMPOS = 4; //Quantidade de campos em uma linha do arquivo.
+    public static final String MSG_PRECO_INVALIDO = "Preço de um produto é inválido.";
+    
+    
+    //Lê um arquivo e retorna uma lista de objetos Preco.
     @Override
-    public List<IAcessoArquivo> ler(FileReader file) throws acessoArquivoException {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public List<Preco> ler(FileReader file) throws acessoArquivoException {
+        List lista = new ArrayList<Preco>();
+        BufferedReader reader = null;
+        String[] campos = null;
+        try{
+            try{
+                reader = new BufferedReader(file);
+                while(reader.ready()){
+                    campos = reader.readLine().split(DELIMITADOR);
+                    verificarPreCondicoes(campos);
+                    lista.add(new Preco(stringToCalendar(campos[POS_DATA]), getPrecosArray(campos)));
+                }
+            } finally{
+                if(reader != null)
+                    reader.close();
+            }
+        } catch(IOException ex){
+            lista = null;
+            throw new acessoArquivoException(MSG_ERRO_ACESSO_ARQUIVO);
+        } catch(acessoArquivoException ex){
+            lista = null;
+            throw new acessoArquivoException(ex.getMessage());
+        }
+        return lista;
     }
 
     @Override
-    public void verificarPreCondicoes(String[] campos) throws acessoArquivoException {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public void verificarPreCondicoes(String[] campos) throws acessoArquivoException{
+        verificarQtdeCamposValidos(campos, QUANT_CAMPOS);
+        verificarValidadeCampos(campos);
+    }
+    
+    //Verifica se os campos são válidos.
+    public void verificarValidadeCampos(String[] campos) throws acessoArquivoException{
+        stringToCalendar(campos[0]); //Verificar data
+        verificarPrecos(campos);
+    }
+    
+    //Verifica se os preços dos produtos são válidos.
+    public void verificarPrecos(String[] campos)throws acessoArquivoException{
+        try{
+            for(int i = PRIMEIRA_POSICAO_PRECO; i < campos.length; i++){
+                if(Double.parseDouble(campos[i]) < 0)
+                    throw new acessoArquivoException(MSG_PRECO_INVALIDO);
+            }
+        } catch(NumberFormatException ex){
+            throw new acessoArquivoException(MSG_PRECO_INVALIDO);
+        }
+    }
+    
+    //Retorna uma lista dos preços dos produtos em um array.
+    public List<Double> getPrecosArray(String[] campos){
+        List<Double> listaPrecos = new ArrayList<Double>();
+        
+        for(int i=PRIMEIRA_POSICAO_PRECO; i < campos.length; i++)
+            listaPrecos.add(Double.parseDouble(campos[i]));
+        return listaPrecos;
     }
 }
