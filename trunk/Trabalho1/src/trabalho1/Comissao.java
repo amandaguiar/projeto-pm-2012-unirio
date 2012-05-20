d078e4d19d9bd9580aa0612263c8a65e7ebd12d6
bianca.melo@uniriotec.br <bianca.melo@uniriotec.br@ad2921a3-afa2-7aa1-14f8-43855f1eb3cd>
2012-05-20 03:25:06 +0000

    A classe comissão foi completamente modificada para facilitar a implementação dos testes parametrizados.

--- trunk/Trabalho1/src/trabalho1/Comissao.java
+++ trunk/Trabalho1/src/trabalho1/Comissao.java
@@ -13,80 +13,13 @@ import java.util.List;
  * @author Jean
  */
 public class Comissao {
-    private int mes;
-    private String codVendedor;
-    private String nomeVendedor;
-    private int categoriaVendedor;
-    private List<Double> valorTotalProdutos;
-    private double valorTotalGeral;
-    private double comissao;
-    private final double cat1ValorLimite1 = 1000;
-    private final double cat1ValorLimite2 = 1800;
-    private final double cat2ValorLimite1 = 2000;
-    private final double cat2ValorLimite2 = 4000;
-
-    public Comissao(int mes, String codVendedor, String nomeVendedor, int categoriaVendedor, List<Double> valorTotalProdutos, double valorTotalGeral, double comissao) {
-        this.mes = mes;
-        this.codVendedor = codVendedor;
-        this.nomeVendedor = nomeVendedor;
-        this.valorTotalProdutos = valorTotalProdutos;
-        this.valorTotalGeral = valorTotalGeral;
-        this.comissao = comissao;
-        this.categoriaVendedor = categoriaVendedor;
-    }
+    private static final double cat1ValorLimite1 = 1000.00;
+    private static final double cat1ValorLimite2 = 1800.00;
+    private static final double cat2ValorLimite1 = 2000.00;
+    private static final double cat2ValorLimite2 = 4000.00;
     
-    public Comissao(){
-        this.mes = 0;
-        this.codVendedor = null;
-        this.nomeVendedor = null;
-        this.valorTotalProdutos = null;
-        this.valorTotalGeral = 0.0;
-        this.comissao = 0.0;
-        this.categoriaVendedor = 0;
-    }
-
-    public String getCodVendedor() {
-        return codVendedor;
-    }
-
-    public void setCodVendedor(String codVendedor) {
-        this.codVendedor = codVendedor;
-    }
-
-    public double getComissao() {
-        return comissao;
-    }
-
-    public void setComissao(double comissao) {
-        this.comissao = comissao;
-    }
-
-    public int getCategoriaVendedor() {
-        return categoriaVendedor;
-    }
-
-    public void setCategoriaVendedor(int categoriaVendedor) {
-        this.categoriaVendedor = categoriaVendedor;
-    }
-    
-    public int getMes() {
-        return mes;
-    }
-
-    public void setMes(int mes) {
-        this.mes = mes;
-    }
-
-    public String getNomeVendedor() {
-        return nomeVendedor;
-    }
-
-    public void setNomeVendedor(String nomeVendedor) {
-        this.nomeVendedor = nomeVendedor;
-    }
-
-    public double getValorTotalGeral() {
-        valorTotalGeral = 0.0;
+    public static double getValorTotalGeral(List<Double> valorTotalProdutos) {
+        double valorTotalGeral = 0.0;
         Iterator it = valorTotalProdutos.iterator();
         while(it.hasNext()){
             valorTotalGeral = valorTotalGeral + Double.parseDouble(it.next().toString());
@@ -94,19 +27,8 @@ public class Comissao {
         return valorTotalGeral;
     }
 
-    public void setValorTotalGeral(double valorTotalGeral) {
-        this.valorTotalGeral = valorTotalGeral;
-    }
-
-    public List<Double> getValorTotalProdutos() {
-        return valorTotalProdutos;
-    }
-
-    public void setValorTotalProdutos(List<Double> valorTotalProdutos) {
-        this.valorTotalProdutos = valorTotalProdutos;
-    }
-    
-    public double calculaComissaoCat1(){
+    public static double calculaComissaoCat1(double valorTotalGeral){
+        double comissao;
         if(valorTotalGeral >= cat1ValorLimite2){
             double valorRestante = valorTotalGeral - cat1ValorLimite2;
             comissao = 220 + (valorRestante*0.2);
@@ -123,7 +45,8 @@ public class Comissao {
         }
     }
     
-    public double calculaComissaoCat2(){
+    public static double calculaComissaoCat2(double valorTotalGeral){
+        double comissao;
         if(valorTotalGeral > cat2ValorLimite2){
         comissao = valorTotalGeral * 0.3;
         return comissao;
@@ -138,12 +61,12 @@ public class Comissao {
         }
     }
     
-    public double calculaComissao(){
+    public static double calculaComissao(int categoriaVendedor, double valorTotalGeral){
         switch (categoriaVendedor){
             case 1:
-                return calculaComissaoCat1();
+                return calculaComissaoCat1(valorTotalGeral);
             case 2:
-                return calculaComissaoCat2();
+                return calculaComissaoCat2(valorTotalGeral);
             default:
                 return 0;
         }
