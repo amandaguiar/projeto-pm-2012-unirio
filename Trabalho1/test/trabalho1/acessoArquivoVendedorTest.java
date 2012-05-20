bbbea2ec36ab7552f2077409daea9d98fee25e59
roberta.aas@gmail.com <roberta.aas@gmail.com@ad2921a3-afa2-7aa1-14f8-43855f1eb3cd>
2012-05-20 04:54:22 +0000

    Teste de arquivo vendedor

---
+++ Trabalho1/test/trabalho1/acessoArquivoVendedorTest.java
@@ -0,0 +1,42 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabalho1;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ *
+ * @author Roberta
+ */
+public class acessoArquivoVendedorTest {
+    public static final String fileOk = ("Arquivos de testes\\acessoArquivoVendedorOkTest.txt");
+   
+    @Test
+    public void LerOkTest() {
+        try{
+            acessoArquivo arquivoVendedor = new acessoArquivoVendedor();
+            List<Vendedor> resultObtido = arquivoVendedor.ler(new File(fileOk));
+            List<Vendedor> resultadoEsperado = null;
+            resultadoEsperado.add(gerarResultadoEsperado("0020","MANOEL DA SILVA",1));
+            resultadoEsperado.add(gerarResultadoEsperado("0021","JOANA ANGELICA",2));
+                      
+            Assert.assertEquals(resultadoEsperado.get(0), resultObtido.get(0));
+            Assert.assertEquals(resultadoEsperado.get(1), resultObtido.get(1));
+
+        } catch(acessoArquivoException ex){
+            Assert.fail();
+        }
+    }
+    public Vendedor gerarResultadoEsperado(String codigoVendedor, String nomeVendedor, int categoriaVendedor) {
+        Vendedor vendedor = new Vendedor(codigoVendedor, nomeVendedor, categoriaVendedor);
+        return vendedor;
+    }
+}
