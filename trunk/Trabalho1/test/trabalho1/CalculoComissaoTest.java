1c61630bad8edf04b385c1736f7dd58388dd2a54
bianca.melo@uniriotec.br <bianca.melo@uniriotec.br@ad2921a3-afa2-7aa1-14f8-43855f1eb3cd>
2012-05-20 04:55:01 +0000

    Classe Comissão refatorada para CalculoComissao.

---
+++ trunk/Trabalho1/test/trabalho1/CalculoComissaoTest.java
@@ -0,0 +1,71 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabalho1;
+
+import junit.framework.Assert;
+import java.util.Arrays;
+import java.util.Collection;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+/**
+ *
+ * @author Bianca
+ */
+
+@RunWith(Parameterized.class)
+public class CalculoComissaoTest {
+    
+    private int categoriaVendedor;
+    private double valorTotalGeral;
+    private double comissao;
+
+    public CalculoComissaoTest(int categoriaVendedor, double valorTotalGeral, double comissao) {
+        this.categoriaVendedor = categoriaVendedor;
+        this.valorTotalGeral = valorTotalGeral;
+        this.comissao = comissao;
+    }
+
+     @Parameterized.Parameters
+    public static Collection<Object []> data() {
+            Object[][] testCalculoComissao = new Object[][] {
+            {1,1.00,0.10},
+            {1,999.99,99.99},
+            {1,1000.0,100.00},
+            {1,1001.00,100.15},
+            {1,1799.99,220.00},
+            {1,1800.00,220.00},
+            {1,1801.00,220.20},
+            {1,2000.00,260.00},
+            {1,4000.00,660.00},
+            {1,10000.00,1860.00},
+            {2,1.00,0.10},
+            {2,1000.00,100.00},
+            {2,1999.99,200.00},
+            {2,2000.00,400.00},
+            {2,3000.00,600.00},
+            {2,4000.00,800.00},
+            {2,4001.00,1200.30},
+            {2,5000.00,1500.00},
+            {2,10000.00,3000.00}
+        };
+        return Arrays.asList(testCalculoComissao);
+    }
+    
+    /**
+     * Test of calculaComissao method, of class CalculoComissao.
+     */
+    @Test
+    public void testCalculaComissao() {
+        try{
+            double resultado = CalculoComissao.calculaComissao(categoriaVendedor, valorTotalGeral);
+            Assert.assertEquals(resultado, comissao);
+        }catch(Exception ex){
+            Assert.fail();
+        }
+    }
+}
+
