cea783586025879eff3c384500ea82565f6176e6
roberta.aas@gmail.com <roberta.aas@gmail.com@ad2921a3-afa2-7aa1-14f8-43855f1eb3cd>
2012-05-21 02:18:36 +0000

    Adicionando novo teste em classe de arquivo vendedor. Criando nova classe de teste para acesso comissão.

---
+++ Trabalho1/test/trabalho1/acessoArquivoComissaoTest.java
@@ -0,0 +1,26 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package trabalho1;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ *
+ * @author Roberta
+ */
+public class acessoArquivoComissaoTest {
+    
+    public static final String arquivoComissao = ("Arquivos de testes\\acessoArquivoComissaoOkTest.txt");
+    public static final String arquivoVendas = ("Arquivos de testes\\acessoArquivoVendasComissaoOkTest.txt");
+    public static final String arquivoVendedores = ("Arquivos de testes\\acessoArquivoVendedorComissaoOkTest.txt");
+    public static final String arquivoPrecos = ("Arquivos de testes\\acessoArquivoPrecosComissaoOkTest.txt");
+    
+}
