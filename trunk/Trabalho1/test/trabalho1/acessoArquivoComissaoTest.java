685b86519b3b5182a90e6e81c6fd93aafee13766
roberta.aas@gmail.com <roberta.aas@gmail.com@ad2921a3-afa2-7aa1-14f8-43855f1eb3cd>
2012-05-21 03:49:34 +0000

    Teste de escrita em arquivo em classe de comissão.

--- trunk/Trabalho1/test/trabalho1/acessoArquivoComissaoTest.java
+++ trunk/Trabalho1/test/trabalho1/acessoArquivoComissaoTest.java
@@ -4,10 +4,10 @@
  */
 package trabalho1;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.*;
 import java.util.ArrayList;
+import java.util.GregorianCalendar;
+import java.util.HashMap;
 import java.util.List;
 import org.junit.Assert;
 import org.junit.Test;
@@ -19,8 +19,62 @@ import org.junit.Test;
 public class acessoArquivoComissaoTest {
     
     public static final String arquivoComissao = ("Arquivos de testes\\acessoArquivoComissaoOkTest.txt");
-    public static final String arquivoVendas = ("Arquivos de testes\\acessoArquivoVendasComissaoOkTest.txt");
-    public static final String arquivoVendedores = ("Arquivos de testes\\acessoArquivoVendedorComissaoOkTest.txt");
-    public static final String arquivoPrecos = ("Arquivos de testes\\acessoArquivoPrecosComissaoOkTest.txt");
+    public static final String arquivoComissaoResultado = ("Arquivos de testes\\acessoArquivoComissaoResultadoTest.txt");
     
+    @Test
+    public void escreverOkTest() {
+        try{
+            HashMap<String, Comissao> comissoes = new HashMap<String, Comissao>();
+            comissoes.put("1234", new Comissao(05, "0020", "MANOEL", 2, 1, 1, 1, 1000, 500, 1000, 2500, 500));
+            
+            acessoArquivoComissao calculo = new acessoArquivoComissao();
+            calculo.escrever(comissoes, "acessoArquivoComissaoResultadoTest.txt");
+            
+            compararArquivos(arquivoComissao, arquivoComissaoResultado);
+             
+        } catch(acessoArquivoException ex){
+            Assert.fail();
+        }
+    }
+    
+    public void compararArquivos(String arquivo1, String arquivo2) throws acessoArquivoException {
+        BufferedReader reader1 = null;
+        BufferedReader reader2 = null;
+        
+        List listaComissoes1 = new ArrayList<Comissao>();
+        List listaComissoes2 = new ArrayList<Comissao>();
+        
+        try {
+            try {
+                reader1 = new BufferedReader(new FileReader(arquivoComissao));
+                reader2 = new BufferedReader(new FileReader(arquivoComissaoResultado));
+                String linha;
+                String[] camposArquivo1 = null;
+                String[] camposArquivo2 = null;
+;
+                while (reader1.ready() && reader2.ready()) {
+                    camposArquivo1 = reader1.readLine().split(";");
+                    camposArquivo2 = reader2.readLine().split(";");
+                    if (camposArquivo1.length != camposArquivo2.length){
+                        throw new acessoArquivoException();
+                    }
+                    
+                    for (int i=0;i<camposArquivo1.length;i++){
+                        if (camposArquivo1 != camposArquivo2){
+                            throw new acessoArquivoException();
+                        }
+                    }
+                }
+            } finally{
+                if(reader1 != null && reader2 != null){
+                    reader1.close();
+                    reader2.close();
+                }
+            }
+        } catch(IOException ex){
+            throw new acessoArquivoException(ex.getMessage());
+        } catch(acessoArquivoException ex){
+            throw new acessoArquivoException(ex.getMessage());
+        }
+    }
 }
